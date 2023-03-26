6216bfa0214dd1830760c07616fb3d23c6619b0c
ASarkar0611 <arunitasarkar18@gmail.com>
2023-03-26 14:01:33 -0400

    Added new problems

--- Problem 1 - Two Sum.py
+++ Problem 1 - Two Sum.py
@@ -12,7 +12,7 @@
 
 # Solution
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
         l = []
         for i in range(len(nums)):
             for j in range(len(nums)):
